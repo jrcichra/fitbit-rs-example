02431510b660d568b6eef6e7f8a9ca86bf3cd16c
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2024-01-09 22:22:14 -0500

    support grabbing from callback

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use anyhow::{Context, Result};
+use axum::extract::{Query, State};
+use axum::routing::get;
+use axum::Router;
 use oauth2::basic::BasicClient;
 use oauth2::reqwest::async_http_client;
 use oauth2::{
     AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, Scope, TokenResponse, TokenUrl,
 };
+use serde::Deserialize;
+use tokio::net::TcpListener;
+use tokio::sync::mpsc::{channel, Sender};
 
 // Fitbit OAuth 2.0 credentials
 const CLIENT_ID: &str = "23RMML";
@@ -11,8 +17,38 @@ const CLIENT_SECRET: &str = "919c54d7f064b7756f00a4b8a26981d0";
 const AUTH_URL: &str = "https://www.fitbit.com/oauth2/authorize";
 const TOKEN_URL: &str = "https://api.fitbit.com/oauth2/token";
 
+// Oauth2 callback query params
+#[derive(Debug, Deserialize)]
+struct Params {
+    code: String,
+    state: String,
+}
+
+#[derive(Clone)]
+struct AppState(Sender<String>);
+
+async fn callback(State(state): State<AppState>, Query(params): Query<Params>) -> String {
+    // send the code through the channel
+    state.0.send(params.code).await.unwrap();
+    "ok".to_string()
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let (sender, mut receiver) = channel::<String>(1);
+
+    // run axum in the background listening for callback requests
+    tokio::spawn(async move {
+        let app = Router::new()
+            .route("/callback", get(callback))
+            .with_state(AppState(sender));
+
+        let bind = format!("0.0.0.0:{}", 8080);
+        let listener = TcpListener::bind(&bind).await.unwrap();
+        println!("listening on {}", &bind);
+        axum::serve(listener, app).await.unwrap();
+    });
+
     // Construct Fitbit OAuth2 client
     let client_id = ClientId::new(CLIENT_ID.to_string());
     let client_secret = ClientSecret::new(CLIENT_SECRET.to_string());
@@ -34,7 +70,10 @@ async fn main() -> Result<()> {
 
     // Simulate a web server that waits for the callback with the authorization code
     // In a real application, you'd set up a web server to handle the redirect and obtain the authorization code
-    let authorization_code = fetch_authorization_code();
+    let authorization_code = receiver
+        .recv()
+        .await
+        .context("recv() did not return data")?;
 
     // Exchange the authorization code for an access token
     let token = client
@@ -48,16 +87,6 @@ async fn main() -> Result<()> {
 
     println!("data: {}", data);
     Ok(())
-}
-
-// Simulate obtaining the authorization code from the callback URL (replace this with your actual code)
-fn fetch_authorization_code() -> String {
-    println!("Paste the authorization code obtained after granting access:");
-    let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    input.trim().to_string()
 }
 
 async fn fetch_heartbeat_data(access_token: &str) -> Result<String> {
